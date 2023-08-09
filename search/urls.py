b49665f6fd0e2849627f3ff3c1d71ad7f9535d4c
rayleigh <rayleigh190@gmail.com>
2023-08-09 15:36:35 +0900

    feat: 각 쇼핑몰에서 첫번째 아이템 주소 반환 API 구현

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path  
 from search.views import parseAPIView
 from search.views import blogAPIView
+from search.views import firstItemAPIView
 
 urlpatterns = [  
     path('parse', parseAPIView.ImageSearchView.as_view()),
     path('search/blog', blogAPIView.BlogView.as_view()),
+    path('search/blog/chatgpt', blogAPIView.BlogSummaryView.as_view()),
+    path('search/first', firstItemAPIView.FirstItem.as_view()),
 ]
--- search/views/blogAPIView.py
+++ search/views/blogAPIView.py
@@ -2,8 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response  
 from rest_framework import status
 import urllib
+import openai
 import os
 import json
+import requests
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
 
 # 시크릿 정보 관리
 import json
@@ -27,6 +31,26 @@ def get_secret(setting, secrets=secrets):
 
 NAVER_CLIENT_ID = get_secret("NAVER_CLIENT_ID")
 NAVER_CLIENT_SECRET = get_secret("NAVER_CLIENT_SECRET")
+OPENAI_KEY = get_secret("OPENAI_KEY")
+
+def chatGPT(text): # 본문 요약
+  openai.api_key = OPENAI_KEY
+  try:
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {
+          "role": "user",
+          "content": text + "이 블로그 리뷰를 요약해줘."
+        }
+      ],
+    )
+  except Exception as e:
+    print('ChatGPT 예외가 발생했습니다.', e)
+    return "fail"
+
+  decoded = completion.choices[0].message["content"]
+  return decoded
 
 class BlogView(APIView):
     
@@ -49,10 +73,48 @@ class BlogView(APIView):
       response = urllib.request.urlopen(request)
     except Exception as e:
       print("네이버 블로그 API 오류 발생.", e)
-      final_result_dic = {'success':False, 'error': "네이버 블로그 API 오류 발생." + e}
+      error_message = "Naver blog API error occurred: " + str(e)
+      final_result_dic = {'success':False, 'error': error_message}
       return Response(final_result_dic, status=200)
 
     response_dic = json.loads(response.read().decode('utf-8'))
     final_result_dic = {'success':True, 'blog': {'reviews':response_dic['items']}, 'error': None}
 
     return Response(final_result_dic, status=200)
+  
+
+class BlogSummaryView(APIView):
+  def get(self, request):
+    # request.data.get('link')
+    link = request.data.get('link')
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+    driver = webdriver.Chrome(options=options)
+    driver.get(link)
+    try:
+      driver.switch_to.frame("mainFrame")
+      html = driver.page_source
+      soup = bs(html, "html.parser")
+      elements = soup.select('div > div.se-main-container')
+    except Exception as e:
+      print("블로그 파싱 에러.", e)
+      error_message = "블로그 파싱 에러 발생: " + str(e)
+      final_result_dic = {'success':False, 'error': error_message}
+      return Response(final_result_dic, status=200)
+
+    blog_main_text = ""
+    #반복문을 사용해서 각 태그의 텍스트값만 출력
+    for tag in elements:
+      blog_main_text += tag.get_text()
+    blog_main_text = blog_main_text.replace("\n", " ")
+    summarized_text = chatGPT(blog_main_text)
+
+    if summarized_text == "fail":
+      final_result_dic = {'success':False, 'error': "블로그 요약을 실패했습니다."}
+      return Response(final_result_dic)
+
+    final_result_dic = {'success':True, 'result': summarized_text, 'error': None}
+
+    return Response(final_result_dic)
